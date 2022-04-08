35bc0bba25a0d6be78e102dbe40469d81d0a6e3a
L2HYUNN <dhl9810@naver.com>
2022-04-09 00:25:59 +0900

    #7.0 To Do List part One

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import Button from "./Button";
-import styles from "./App.module.css";
-import { useState, useEffect } from "react";
-
-function Hello() {
-  const byeFn = () => console.log("Destroied :)");
-  const hiFn = () => {
-    console.log("Created :)");
-    return byeFn;
-  };
-  useEffect(hiFn, []);
-  return <h1>Hello</h1>;
-}
+import { useState } from "react";
 
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  function Li(toDo) {
+    return <li>{toDo}</li>;
+  }
+  const onChangeToDo = (e) => {
+    setToDo(e.target.value);
+  };
+  const onSubmitToDo = (e) => {
+    e.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    setToDo("");
+    setToDos((currentArray) => [toDo, ...currentArray]);
+    console.log(toDos);
+  };
+
+  const onSubmitCompleted = () => {};
   return (
     <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? "Hide" : "Showing"}</button>
+      <h1>Your To Do List First! ({toDos.length})</h1>
+      <form onSubmit={onSubmitToDo}>
+        <input
+          onChange={onChangeToDo}
+          value={toDo}
+          type="text"
+          placeholder="Input your to do"
+        ></input>
+        <button type="submit">Submit</button>
+      </form>
+      {/* <ul>{toDo ? <Li /> : null}</ul> */}
+      <div>Your Completed List First!</div>
     </div>
   );
 }
