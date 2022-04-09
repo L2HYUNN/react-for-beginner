57c693c1aae50a9bc49ef0b00bf9efdb3630061a
L2HYUNN <dhl9810@naver.com>
2022-04-09 21:14:33 +0900

    #7.6 Parameters Code Challenge

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,18 +1,41 @@
-import { useEffect } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import Movies from "../components/Movie";
 
 function Detail() {
   const { id } = useParams();
-  const getMovie = async () => {
-    const json = await (
+  const [loading, setLoading] = useState(true);
+  const [movieInfo, setMovieInfo] = useState({});
+
+  const getMovie = useCallback(async () => {
+    const {
+      data: { movie },
+    } = await (
       await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
     ).json();
-    console.log(json);
-  };
+    setMovieInfo(movie);
+    setLoading(false);
+  }, [id]);
+
   useEffect(() => {
     getMovie();
-  }, []);
-  return <h1>Detail</h1>;
+  }, [getMovie]);
+
+  return (
+    <div>
+      {loading ? (
+        <h1>Loading</h1>
+      ) : (
+        <Movies
+          id={movieInfo.id}
+          mediumCoverImage={movieInfo.medium_cover_image}
+          title={movieInfo.title}
+          summary={movieInfo.description_full}
+          genres={movieInfo.genres}
+        />
+      )}
+    </div>
+  );
 }
 
 export default Detail;
