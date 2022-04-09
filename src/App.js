4a6fbbfd6540a6058ca3c6dbd6a131aa0297593c
L2HYUNN <dhl9810@naver.com>
2022-04-09 19:10:59 +0900

    #7.5 React Router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import Home from "./routes/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Detail from "./routes/Detail";
 
 function App() {
   return (
-    <div>
-      <Home />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/movie" element={<Detail />}></Route>
+        <Route path="/" element={<Home />}></Route>
+      </Routes>
+    </Router>
   );
 }
 
